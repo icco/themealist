b90fbb39b87cded70bba328c8b7d8492dad1a8db
Nat Welch <nat@natwelch.com>
2011-06-30 03:51:30 +0100

    Bringing in my basic site template.

---
+++ site.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# An app for ...
+# @author Nat Welch - https://github.com/icco
+
+begin
+   require "rubygems"
+rescue LoadError
+   puts "Please install Ruby Gems to continue."
+   exit
+end
+
+# Check all of the gems we need are there.
+[ "sinatra", "less", "sequel" ].each {|gem|
+   begin
+      require gem
+   rescue LoadError
+      puts "The gem #{gem} is not installed.\n"
+      exit
+   end
+}
+
+configure do
+   set :sessions, true
+   DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://data.db')
+end
+
+get '/' do
+   erb :index, :locals => {}
+end
+
+post '/' do
+   redirect '/'
+end
+
+class Entry < Sequel::Model(:entries)
+end
