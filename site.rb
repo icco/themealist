21bacdee61169316ce3a9a0ca20acd04650c0b41
Nat Welch <nat@natwelch.com>
2011-09-12 00:21:59 -0700

    a little time_since hack.

--- site.rb
+++ site.rb
@@ -71,3 +71,20 @@ end
 class Item < Sequel::Model(:items)
    many_to_one :meal
 end
+class Time
+   def time_since
+      dist = Time.now - self
+      distance = dist.abs
+      post_sentence = dist < 0 ? "" : " ago"
+
+      out = case distance
+            when 0 .. 59 then "#{distance} seconds"
+            when 60 .. (60*60) then "#{distance/60} minutes"
+            when (60*60) .. (60*60*24) then "#{distance/(60*60)} hours"
+            when (60*60*24) .. (60*60*24*30) then "#{distance/((60*60)*24)} days"
+            else self.to_s
+            end
+
+      return out.sub(/^1 (\w+)s ago$/, '1 \1 ago')
+   end
+end
